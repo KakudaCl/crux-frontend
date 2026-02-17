ce15d1420be50730981b4527ac8c5929bf5190ab
角田 直樹 <kakuda@iret.co.jp>
2026-02-17 12:37:13 +0900

    refactor: ヘッダーのホバーまわりの設定を変更(項目毎に異なる色付け)

--- crux-app/src/components/ResultPage.jsx
+++ crux-app/src/components/ResultPage.jsx
@@ -165,15 +165,15 @@ export const ResultPage = () => {
       <header className="result-page__header">
         <div className="result-page__header-logo">BOLLOG</div>
         <nav className="result-page__header-nav">
-          <Link to="/month" className="result-page__nav-link result-page__nav-link--active">
+          <Link to="/month" className="result-page__nav-link result-page__nav-link--month result-page__nav-link--active">
             <img src={GreenHoldImage} style={{ width: '20px', marginRight: '10px' }} alt="マンスリー別完登率" />
             マンスリー別完登率
           </Link>
-          <Link to="/area" className="result-page__nav-link">
+          <Link to="/area" className="result-page__nav-link result-page__nav-link--area">
             <img src={BlueHoldImage} style={{ width: '20px', marginRight: '10px' }} alt="エリア別完登率" />
             エリア別完登率
           </Link>
-          <Link to="/trylog" className="result-page__nav-link">
+          <Link to="/trylog" className="result-page__nav-link result-page__nav-link--trylog">
             <img src={YellowHoldImage} style={{ width: '17px', marginRight: '10px' }} alt="トライログ" />
             トライログ
           </Link>
--- crux-app/src/components/ResultPageArea.jsx
+++ crux-app/src/components/ResultPageArea.jsx
@@ -161,15 +161,15 @@ export const ResultPageArea = () => {
       <header className="result-page__header">
         <div className="result-page__header-logo">BOLLOG</div>
         <nav className="result-page__header-nav">
-          <Link to="/month" className="result-page__nav-link">
+          <Link to="/month" className="result-page__nav-link result-page__nav-link--month">
             <img src={GreenHoldImage} style={{ width: '20px', height: '20px', marginRight: '10px' }} alt="マンスリー別完登率" />
             マンスリー別完登率
           </Link>
-          <Link to="/area" className="result-page__nav-link result-page__nav-link--active">
+          <Link to="/area" className="result-page__nav-link result-page__nav-link--area result-page__nav-link--active">
             <img src={BlueHoldImage} style={{ width: '20px', height: '20px', marginRight: '10px' }} alt="エリア別完登率" />
             エリア別完登率
           </Link>
-          <Link to="/trylog" className="result-page__nav-link">
+          <Link to="/trylog" className="result-page__nav-link result-page__nav-link--trylog">
             <img src={YellowHoldImage} style={{ width: '17px', marginRight: '10px' }} alt="トライログ" />
             トライログ
           </Link>
